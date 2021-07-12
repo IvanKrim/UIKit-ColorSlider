32d3c0e199ab742dd4259e4529e6dd0280918e3c
Ivan Kalabishka <reamonn96@gmail.com>
2021-07-12 15:04:52 +0300

    ready

--- UIKit-ColorSlider/ViewController.swift
+++ UIKit-ColorSlider/ViewController.swift
@@ -20,11 +20,6 @@ class ViewController: UIViewController {
     @IBOutlet var greenSlider: UISlider!
     @IBOutlet var blueSlider: UISlider!
     
-    private var redSliderValue: CGFloat = 0.50
-    private var greenSliderValue: CGFloat = 0.50
-    private var blueSliderValue: CGFloat = 0.50
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .opaqueSeparator
@@ -32,45 +27,64 @@ class ViewController: UIViewController {
         redSlider.minimumTrackTintColor = .red
         greenSlider.minimumTrackTintColor = .green
         
-        slidersValueLabael()
-        setColor()
+        setColor() // 3
+        setValue(for: redLabel, greenLabel, blueLabel) // 5
     }
     
-    @IBAction func redSliderActrion() {
-        redSliderValue = CGFloat(redSlider.value)
-        slidersValueLabael()
-        setColor()
+    // 2
+    @IBAction func slidersAction(_ sender: UISlider) {
+        setColor() // 3
+        
+        // 6
+        switch sender {
+        case redSlider: setValue(for: redLabel)
+        case greenSlider: setValue(for: greenLabel)
+        default: setValue(for: blueLabel)
+        }
     }
-    
-    @IBAction func greenSliderAction() {
-        greenSliderValue = CGFloat(greenSlider.value)
-        slidersValueLabael()
-        setColor()
-    }
-    
-    @IBAction func blueSliderAction() {
-        blueSliderValue = CGFloat(blueSlider.value)
-        slidersValueLabael()
-        setColor()
-    }
-    
-    
-    
 }
+
+// MARK: - Extension
 extension ViewController {
+    // 3
     private func setColor() {
         viewDisplay.backgroundColor = UIColor(
-            red: redSliderValue,
-            green: greenSliderValue,
-            blue: blueSliderValue,
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
             alpha: 1
         )
     }
     
-    private func slidersValueLabael() {
-        redLabel.text = String(format: "%.2f", redSlider.value)
-        greenLabel.text = String(format: "%.2f", greenSlider.value)
-        blueLabel.text = String(format: "%.2f", blueSlider.value)
+    //
+    //    private func slidersValueLabael() {
+    //        redLabel.text = String(format: "%.2f", redSlider.value)
+    //        greenLabel.text = String(format: "%.2f", greenSlider.value)
+    //        blueLabel.text = String(format: "%.2f", blueSlider.value)
+    //    }
+    //
+    
+    // 5
+    private func setValue(for labels: UILabel...) {
+        labels.forEach { label in
+            switch label {
+            case redLabel:
+                label.text = string(from: redSlider)
+            case greenLabel:
+                label.text = string(from: greenSlider)
+            default:
+                label.text = string(from: blueSlider)
+            }
+        }
     }
+    
+    // 4
+    // Работа со слайдером
+    private func string(from slider: UISlider) -> String {
+        String(format: "%.2f", slider.value)
+    }
+    
+    
+    
+    
 }
-
